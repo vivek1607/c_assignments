7426818bd1ea6902dcf187f9193c965c30c61c4c
vivek1607 <55707156+vivek1607@users.noreply.github.com>
2019-09-23 21:55:34 +0530

    Add files via upload

---
+++ Q1Assign1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int length,breadth,area;
+    printf("enter length and breadth of rectangle whose area to be find");
+    scanf("%d%d",&length,&breadth);
+    area = length*breadth;
+    printf("the area of rectangle is : %d",area);
+
+}
